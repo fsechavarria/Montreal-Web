451e22d8f1e5bca247b2defe0f7301a2cd69a304
fsechavarria <fsechavarria@gmail.com>
2018-10-16 15:02:36 -0300

    Redireccion a login en caso de no estar con la sesion iniciada

--- src/main/java/controller/LandingController.java
+++ src/main/java/controller/LandingController.java
@@ -1,6 +1,9 @@
 
 package controller;
 
+import entities.AuthUser;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +13,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LandingController {
     
     @RequestMapping(value = "/home.htm", method = RequestMethod.GET)
-    public String loginRequest (Model model) {
+    public String loginRequest (HttpServletRequest request, Model model) {
+        // Se verifica si la sesion existe, en caso contrario se redirecciona al login.
+        HttpSession session = request.getSession(false); // False evita que se cree una nueva sesion si esta no existe
+        if (session == null) {
+            return "redirect:/login.htm";
+        } else {
+            AuthUser aU = (AuthUser)session.getAttribute("loggedUser");
+            if (aU == null) {
+                return "redirect:/login.htm";
+            }
+        }
+        
         model.addAttribute("title", "Home");
         return "landing";
     }
